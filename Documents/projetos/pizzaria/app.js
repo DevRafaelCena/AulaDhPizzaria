9bbe52c2023712ed21c2ea0e70a2a01e996bc713
DevRafaelCena <rafael.cena@hotmail.com>
2020-02-20 15:54:33 -0300

    inicial da pizzaria

---
+++ Documents/projetos/pizzaria/app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+let app = express()
+//const rotas = require("./routes/rotasProduto")
+
+
+//app.use("/produtos", rotas)
+
+app.listen(3000,()=>{
+    console.log("subiuu!")
+})
+
