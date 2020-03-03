32f0130b23d175305b6b456b0c71f248d543b193
DevRafaelCena <rafael.cena@hotmail.com>
2020-03-03 15:01:41 -0300

    inicial

---
+++ Documents/projetos/pizzaria/controllers/PizzariaController.js
@@ -0,0 +1,13 @@
+const PizzariaController ={
+    criarProduto: (req,res)=>{
+        res.send("Criando um novo Produto!!!!")
+
+    },
+    deletarProduto: (req,res)=>{
+        const {id} = req.params
+        res.send("APagando o produto de id: " + id)
+
+    }
+}
+
+module.exports = ProdutoController
